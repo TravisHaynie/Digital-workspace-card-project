9129a942c6e436dbf1d1bcaaca09e07499c90341
TravisHaynie <travihay@gmail.com>
2025-11-21 11:49:56 -0800

    B:changed image

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,9 @@
+import { profilePic } from "src/assets/img/profile-pic2.jpg"
+
 export default function Info() {
     return(
         <header>
-            <img src="src/assets/img/profile-pic2.jpg"/>
+            <img src={profilePic} alt="profile picture of a man in a suite in black and gray"/>
             <h1>Travis Haynie</h1>
             <p>Frontend Developer</p>
             <p>Travihay.website</p>
