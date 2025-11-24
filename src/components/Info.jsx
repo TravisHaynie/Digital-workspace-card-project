6b49ab8a280c063ed53df52045a3fede51bd621b
TravisHaynie <travihay@gmail.com>
2025-11-24 13:29:26 -0800

    L:added more code

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,6 @@
 import  profilePic  from "../assets/img/profile-pic2.jpg"
 import email from "../assets/img/Icon.png"
-import linkdln from "../assets/img/link2.png"
+import linkdln from "../assets/img/link.png"
 
 export default function Info() {
     return(
@@ -11,7 +11,7 @@ export default function Info() {
                 <p className="profession margin">Frontend Developer</p>
                 <p className="website margin">Travihay.website</p>
                 <button className="email-btn btn-padding"><span className="span"><img className="img-size" src={email}></img><a href="mailto:travihay@gmail.com" className="color-black">Email</a></span></button>
-                <button className="linkedln-btn btn-padding"><span className="span"><img  className="img-size" src={linkdln}></img><a href="https://www.linkedin.com/in/travis-haynie" className="color-white">LinkedIn</a></span></button>
+                <button className="linkedln-btn btn-padding"><span className="span"><img  className="img-size link-border" src={linkdln}></img><a href="https://www.linkedin.com/in/travis-haynie" className="color-white">LinkedIn</a></span></button>
             </div>
         </header>
     )
